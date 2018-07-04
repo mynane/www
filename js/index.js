23aa132b3197f76aa76ebb6d35bdf1b509c60ca9
jikequan <jikequan@163.com>
2018-07-04 15:23:50 +0800

    update

--- js/index.js
+++ js/index.js
@@ -16,6 +16,23 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+function check_update() {
+    chcp.fetchUpdate(function(error, data) {
+        var updateBtn = document.querySelector("#update");
+        if(!error) {
+            updateBtn.innerHTML = "立即更新";
+            updateBtn.addEventListener("click", function(event) {
+                updateBtn.innerHTML = "正在升级，升级完毕应用将自动重启...";
+                chcp.installUpdate(function(error) {
+                    updateBtn.innerHTML = "更新完成";
+                })
+            })
+        } else {
+            updateBtn.innerHTML = "你当前是最新版本";
+        }
+    })
+}
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -28,22 +45,7 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-        document.querySelector("#check_update").addEventListener("click", function(e) {
-            chcp.fetchUpdate(function(error, data) {
-                var updateBtn = document.querySelector("#update");
-                if(!error) {
-                    updateBtn.innerHTML = "立即更新";
-                    updateBtn.addEventListener("click", function(event) {
-                        updateBtn.innerHTML = "正在升级，升级完毕应用将自动重启...";
-                        chcp.installUpdate(function(error) {
-                            updateBtn.innerHTML = "更新完成";
-                        })
-                    })
-                } else {
-                    updateBtn.innerHTML = "你当前是最新版本";
-                }
-            })
-        })
+        document.querySelector("#check_update").addEventListener("click", check_update)
     },
 
     // Update DOM on a Received Event
