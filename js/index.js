a58ef0a8692107b12fba8119f01e2f3aca08349c
jikequan <jikequan@163.com>
2018-07-04 15:51:24 +0800

    update

--- js/index.js
+++ js/index.js
@@ -23,8 +23,10 @@ function check_update() {
             updateBtn.innerHTML = "立即更新";
             updateBtn.addEventListener("click", function(event) {
                 updateBtn.innerHTML = "正在升级，升级完毕应用将自动重启...";
-                chcp.installUpdate(function(error) {
-                    updateBtn.innerHTML = "更新完成";
+                setTimeout(function() {
+                    chcp.installUpdate(function(error) {
+                        updateBtn.innerHTML = "更新完成";
+                    })
                 })
             })
         } else {
