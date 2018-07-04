d963b061454aa7c0fc353d76bae746c9f7cf9f54
jikequan <jikequan@163.com>
2018-07-04 15:06:56 +0800

    update

--- js/index.js
+++ js/index.js
@@ -28,6 +28,20 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        chcp.fetchUpdate(function(error, data) {
+            var updateBtn = document.querySelector("#update");
+            if(!error) {
+                updateBtn.innerHTML = "立即更新";
+                updateBtn.addEventListener("click", function(event) {
+                    updateBtn.innerHTML = "正在升级，升级完毕应用将自动重启...";
+                    chcp.installUpdate(function(error) {
+                        updateBtn.innerHTML = "更新完成";
+                    })
+                })
+            } else {
+                updateBtn.innerHTML = "你当前是最新版本";
+            }
+        })
     },
 
     // Update DOM on a Received Event
